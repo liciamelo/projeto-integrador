c22edeb09ea5abdd0876393e529def4cc90ed829
liciamelo <liciamelooliveira@gmail.com>
2025-07-08 16:28:24 -0300

    feat: add delete, update and details scripts for imigrante

--- imigrante.py
+++ imigrante.py
@@ -60,3 +60,80 @@ def ListarTodosImigrantes():
 
         cursor.close()
         conn.close()
+
+
+def DetalheImigrante(id):
+    print("***************************************")
+    print("Cadastrar imigrante")
+    print("***************************************")
+    print("")
+
+    # criando conexão
+    conn = Conectar()
+    cursor = conn.cursor()
+
+    # a vírgula no final é pro py entender que é uma lista
+    cursor.execute(
+        'SELECT IDIMIGRANTE, NOME, NACIONALIDADE, DT_NASCIMENTO, DOCUMENTO FROM IMIGRANTE WHERE IDIMIGRANTE = %s', (id,))
+    for id, nome, nacionalidade, dt_nascimento, documento in cursor.fetchall():
+        print(f"ID: {id}")
+        print(f"Nome: {nome}")
+        print(f"Nacionalidade: {nacionalidade}")
+        print(f"Data de Nascimento: {dt_nascimento}")
+        print(f"Documento: {documento}")
+        print("")
+
+    cursor.close()
+    conn.close()
+
+    input("Pressione enter para voltar ao menu")
+
+
+def AlterarImigrante(id):
+    print("***************************************")
+    print("Alterando imigrante")
+    print("***************************************")
+    print("")
+
+    print("Informe os novos valores do imigrante: ")
+    nome = input("Informe o nome")
+    nacionalidade = input("Informe a nacionalidade: ")
+    dt_nascimento = input("Informe a data de nascimento: ")
+    documento = input("Informe o documento: ")
+
+    # criando conexão
+    conn = Conectar()
+    cursor = conn.cursor()
+
+    # enviando o comando para o banco de dados para alterar o imigrante
+    cursor.execute("UPDATE IMIGRANTE SET NOME = %s, NACIONALIDADE = %s, DT_NASCIMENTO = %s, DOCUMENTO = %s WHERE IDIMIGRANTE = %s",
+                   (nome, nacionalidade, dt_nascimento, documento, id))
+
+    conn.commit()
+
+    conn.close()
+    conn.close()
+
+    print("Imigrante atualizado com suceso")
+    input("Pressione enter para voltar ao menu")
+
+
+def ExcluirImigrante(id):
+    print("***************************************")
+    print("Excluindo imigrante")
+    print("***************************************")
+    print("")
+
+    confirmar = input(
+        "Deseja realmente excluir o imigrante? (informe 1 para confirmar: ")
+    if (confirmar == '1'):
+       # criando conexao
+        conn = Conectar()
+        cursor = conn.cursor()
+
+    # enviando comando sql para excluir o imigrante
+    cursor.execute("DELETE FROM IMIGRANTE WHERE IDIMIGRANTE = %s", (id,))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
--- imigranteTela.py
+++ imigranteTela.py
@@ -1,5 +1,5 @@
 import os
-from imigrante import NovoImigrante, ListarTodosImigrantes
+from imigrante import NovoImigrante, ListarTodosImigrantes, DetalheImigrante, AlterarImigrante, ExcluirImigrante
 
 
 def LimpaTela():
@@ -16,15 +16,35 @@ def ImigranteMenu():
         print(" === Menu Imigrante ===")
         print("1. Cadastrar imigrante")
         print("2. Listar imigrante")
+        print("3 - Ver datalhes do imigrante")
+        print("4 - Alterar imigrante")
+        print("5 - Excluir imigrante")
         print("9. Sair")
 
-        opcao = input("Escolha uma opção:")
+        opcao = input("Escolha uma opção: ")
 
         if opcao == '1':
+            # chamando o metodo que cadastra um novo imigrante
             NovoImigrante()
+            # chamando o metodo pra listar todos os imigrantes
         elif opcao == '2':
             ListarTodosImigrantes()
             input("Pressione enter para continuar...")
+        elif opcao == '3':
+            # chamando o metodo que lista todos os imigrantes para que o usuario escolha um
+            ListarTodosImigrantes()
+            id = input("Informe o id do imigrante para ver detalhes: ")
+        elif opcao == '4':
+           # chamando o metodo para listar todos os iigrantes para escolher um
+            ListarTodosImigrantes()
+            print("")
+            id = input("Informe o id do imigrante para alterar: ")
+            AlterarImigrante(id)
+            # chamando o metodo que mostra os detalhes do imigrante
+            DetalheImigrante(id)
+        elif opcao == '5':
+            ExcluirImigrante()
+            id = input("Informe o id do imigrante para excluir: ")
         elif opcao == '9':
             break
         else:
